dcc2e8061f6d955fe302022f69d13fb8efee6cfb
Mark Wong Siang Kai <mwong@palantir.com>
2018-07-29 15:11:58 -0700

    Added barebones

---
+++ cmd/cmd.go
@@ -0,0 +1,5 @@
+package cmd
+
+func Execute() int {
+	return 0
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"os"
+
+	"github.com/markwongsk/go-k8slint/cmd"
+)
+
+func main() {
+	os.Exit(cmd.Execute())
+}
